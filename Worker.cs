b68eb49d6e93058739781b03a92c89bb262d2f6c
ande9823 <55538317+ande9823@users.noreply.github.com>
2019-09-19 13:40:13 +0200

    Add files via upload

---
+++ Worker.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Worker : MonoBehaviour
+{
+    Clicker clickerScript;
+    int counter;
+    // Start is called before the first frame update
+    void Start()
+    {
+        clickerScript = GetComponent<Clicker>();
+    }
+
+    private void FixedUpdate()
+    {
+        counter++;
+        if (counter == 50)
+        {
+            clickerScript.score++;
+            counter = 0;
+        }
+       
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("+1 cookie");
+        //clickerScript.score++;
+    }
+}
